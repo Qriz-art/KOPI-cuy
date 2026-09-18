876cd7ae60b043c6c9408f713cc05726fdf4f8bf
Qriz-art <adioebipu@gmail.com>
2026-09-18 20:43:34 +0700

    hapus

--- app/robots.js
+++
@@ -1,9 +0,0 @@
-import { siteUrl } from "@/lib/site";
-
-export default function robots() {
-  return {
-    rules: [{ userAgent: "*", allow: "/" }],
-    sitemap: `${siteUrl}/sitemap.xml`,
-    host: siteUrl,
-  };
-}
